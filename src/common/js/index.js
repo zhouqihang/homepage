c814eb3142db1c24c03962a5b89b61f78b060a82
zhouqihang <console_log@126.com>
2018-01-14 21:03:46 +0800

    :fire:移除链接和畅言评论系统

--- src/common/js/index.js
+++ src/common/js/index.js
@@ -1,34 +1,33 @@
-$(function () {
+$(function() {
 
     // 左侧滚动判断
     var $left = $('#left'), // 左边div
         $leftContent = $left.children(), // 左边div的内容
         docHeight = $(document).height(), // 文档高度
         isSmallScreen = false, // 是否是小屏幕，此时不需要计算位置
-        isCenter = false, // 是否已经居中
-        $link = $('#link'); // 链接
+        isCenter = false; // 是否已经居中
 
     // 菜单
-    var $nav = $('#nav'),       // navBox
-        $navHandle = $('.nav-handle');  // 小屏幕触发组件
+    var $nav = $('#nav'), // navBox
+        $navHandle = $('.nav-handle'); // 小屏幕触发组件
 
     /**
      * 窗口大小调整时，判断是否是小屏幕
      * 根据页面元素的定位属性判断
      */
-    $(window).on('resize', wait(200, function () {
+    $(window).on('resize', wait(200, function() {
         isSmallScreen = 'fixed' != $left.css('position')
     })).trigger('resize');
 
     /**
      * 页面滚动时判断位置
      */
-    $(window).on('scroll', wait(200, function () {
+    $(window).on('scroll', wait(200, function() {
         // 小屏幕
         if (isSmallScreen) return;
         // 如果屏幕过小，则不必执行以下操作
         // 判断left-content 是否完全显示
-        if ($left.height() <  $leftContent.height()) {
+        if ($left.height() < $leftContent.height()) {
             // 没有完全显示
             // 页面滚动的距离 / 页面超出的距离 = left-content移动的距离 / left-content超出的距离
             var docOffsetY = docHeight - $(window).height(), // 文档超出的高度
@@ -38,16 +37,7 @@ $(function () {
             $leftContent.animate({
                 top: -contentOffsetTop
             });
-            // 将连接设置到页面的底部
-            $link.css({
-                top: $(window).height() - $link.innerHeight() + $(window).scrollTop()
-            });
         } else {
-            // 完全显示，将链接设置到左侧内容底部对齐
-            $link.css({
-                top: $leftContent.offset().top + $leftContent.innerHeight() - $link.innerHeight()
-            });
-
             // 将左侧内容区域垂直居中,保证只操作一次
             if (isCenter) return;
             $leftContent.css('top', ($left.height() - $leftContent.height()) / 2);
@@ -63,14 +53,14 @@ $(function () {
      */
     function wait(time, fn) {
         var timer = null;
-        return function () {
+        return function() {
             clearTimeout(timer);
             timer = setTimeout(fn, time);
         }
     }
 
     // 屏幕变化时的菜单处理
-    $(window).on('scroll', function () {
+    $(window).on('scroll', function() {
         var offsetTop = $(window).scrollTop();
         // 小屏幕，调整菜单的位置
         if (isSmallScreen) {
@@ -91,7 +81,7 @@ $(function () {
     // 点赞动画
     var $starHandle = $('#star-handle'),
         $star = $('#star');
-    $starHandle.on('click', function () {
+    $starHandle.on('click', function() {
         if ($star.isStared) {
             $star.removeClass('stared star-end');
             $star.isStared = false;
@@ -100,13 +90,4 @@ $(function () {
             $star.isStared = true;
         }
     });
-
-    // 显示隐藏链接
-    var $linkHandle = $('#link-handle');
-        $linkHandle.mouseenter(function () {
-            $link.fadeToggle();
-        });
-        $link.mouseleave(function () {
-            $(this).fadeToggle();
-        })
 });
