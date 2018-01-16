2f4646613dda8bd356b3157ad5ac12e215f7e4a0
zhouqihang <console_log@126.com>
2018-01-16 22:33:59 +0800

    add star

--- src/article/index.js
+++ src/article/index.js
@@ -1,3 +1,27 @@
 import '../common/js/index';
 import "./index.less";
 import "github-markdown-css/github-markdown.css";
+import axios from 'axios';
+
+let star = document.querySelector('#star');
+console.log(star);
+
+let $star = $('#star');
+let hasStard = false;
+$star.on('click', function() {
+    if (hasStard) {
+        return;
+    }
+    axios.post('/star/281').then(res => {
+        hasStard = true;
+    }).catch(err => {
+        // nothing...
+    });
+    if ($star.isStared) {
+        $star.removeClass('stared star-end');
+        $star.isStared = false;
+    } else {
+        $star.addClass('stared star-end');
+        $star.isStared = true;
+    }
+})
