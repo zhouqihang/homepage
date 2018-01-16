278553c35a50e42ad971bd56c1e623bceeaab6a6
zhouqihang <console_log@126.com>
2018-01-16 22:49:08 +0800

    star done

--- src/article/index.js
+++ src/article/index.js
@@ -3,16 +3,17 @@ import "./index.less";
 import "github-markdown-css/github-markdown.css";
 import axios from 'axios';
 
-let star = document.querySelector('#star');
-console.log(star);
-
 let $star = $('#star');
 let hasStard = false;
 $star.on('click', function() {
     if (hasStard) {
         return;
     }
-    axios.post('/star/281').then(res => {
+    let id = $star.data('article');
+    if (!id) {
+        return;
+    }
+    axios.post('/star/' + id).then(res => {
         hasStard = true;
     }).catch(err => {
         // nothing...
